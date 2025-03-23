f535f28a61407f0153d6ca12573819afbb1f43c7
LiWinDom <wadim.gorbachev@mail.ru>
2025-03-23 14:44:22 +0300

    Fixed theme changing

--- application/controllers/controller_theme.php
+++ application/controllers/controller_theme.php
@@ -2,27 +2,33 @@
 
 class Controller_Theme extends Controller
 {
+    private function return_back() {
+        $url = $_SERVER["HTTP_REFERER"] ?? "/";
+        header("Location: " . $url);
+        exit();
+    }
+
     public function action_dark()
     {
         setcookie("theme", "dark", 2147483647, "/");
-        header("Location: " . $_SERVER["HTTP_REFERER"]);
+        $this->return_back();
     }
 
     public function action_dark_monochrome()
     {
         setcookie("theme", "dark_monochrome", 2147483647, "/");
-        header("Location: " . $_SERVER["HTTP_REFERER"]);
+        $this->return_back();
     }
 
     public function action_light()
     {
         setcookie("theme", "light", 2147483647, "/");
-        header("Location: " . $_SERVER["HTTP_REFERER"]);
+        $this->return_back();
     }
 
     public function action_light_monochrome()
     {
         setcookie("theme", "light_monochrome", 2147483647, "/");
-        header("Location: " . $_SERVER["HTTP_REFERER"]);
+        $this->return_back();
     }
 }
